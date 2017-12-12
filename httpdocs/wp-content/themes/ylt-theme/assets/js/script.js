f9054a981923f0674634f36c9c708893ab4394ce
Simon <simon@ylt.nl>
2017-12-12 17:36:36 +0100

    Bestellen

--- httpdocs/wp-content/themes/ylt-theme/assets/js/script.js
+++ httpdocs/wp-content/themes/ylt-theme/assets/js/script.js
@@ -1,15 +1,47 @@
 "use strict";
+
+
+
+
 jQuery(function () {
 
-    var ScrollMagic = require('scrollmagic');
-    var controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({
-        duration: 100,	// the scene should last for a scroll distance of 100px
-        offset: 50		// start this scene after scrolling for 50px
-    })
-        .setPin("#voordelen-op-een-rij") // pins the element for the the scene's duration
-        .addTo(controller); // assign the scene to the controller
+
+    jQuery(document).ajaxComplete(function () {
+        var bestellen_grid = jQuery('#bestellen-grid');
+        if(bestellen_grid.length > 0){
+            var accessoire = jQuery('.bestellen',bestellen_grid);
+            accessoire.each(function () {
+                var min = jQuery(this).find('.min');
+                var plus = jQuery(this).find('.plus');
+                var aantal_veld = jQuery(this).find('.aantal-veld input');
+
+                min.on('click',function () {
+                    if(aantal_veld.val() > 0){
+                        aantal_veld.val(Number(aantal_veld.val()) - 1);
+                    }
+                });
+                plus.on('click',function () {
+                    aantal_veld.val(Number(aantal_veld.val()) + 1);
+                });
+
+                min.addClass('nul');
+                aantal_veld.addClass('nul');
+                jQuery(this).on('click',function () {
+                    console.log(aantal_veld.val());
+                    if(aantal_veld.val() <= 0){
+                        min.addClass('nul');
+                        aantal_veld.addClass('nul');
+                    }
+                    else{
+                        min.removeClass('nul');
+                        aantal_veld.removeClass('nul');
+                    }
+                });
+
+            });
+        }
+    });
 
 });
 
